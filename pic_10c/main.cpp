8e29b61b3cdff43dc2c9116fb5c52a201e1245b6
amyfei2015 <feiqiang2015apply@gmail.com>
2017-10-11 00:03:50 -0700

    Initial Commit

---
+++ pic_10c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pic_10c
+//
+//  Created by qiang fei on 2017/10/11.
+//  Copyright © 2017年 qiang fei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
