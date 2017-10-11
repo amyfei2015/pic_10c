7c373344117f358cae695eae3e8cd04729e79391
amyfei2015 <feiqiang2015apply@gmail.com>
2017-10-11 00:12:13 -0700

    It is a try.

--- pic_10c/main.cpp
+++ pic_10c/main.cpp
@@ -1,15 +1,23 @@
-//
-//  main.cpp
-//  pic_10c
-//
-//  Created by qiang fei on 2017/10/11.
-//  Copyright © 2017年 qiang fei. All rights reserved.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main(){
+    /* --STATEMENTS-- */
     return 0;
 }
